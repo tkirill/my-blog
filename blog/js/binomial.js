02083165dc58d118ad29a09062650cdd02edb1b8
tkirill <tkirill128@gmail.com>
2014-03-10 00:40:48 +0600

    Add chart of binomial distribution.
    Use morris.js for client-side charts.

--- blog/js/binomial.js
+++ blog/js/binomial.js
@@ -8,30 +8,41 @@ function BinomialViewModel() {
         var result = [];
 
         for (var k = 0; k <= self.totalNumberOfExperiments(); k++)
-            result[k] = self.computeForK(k);
+            result.push({ns: k, probability: self.computeForK(k)});
 
-        $('#result').text(result);
-	}
+        self.renderChart(result);
+	};
 
     self.computeForK = function(k) {
         var n = self.totalNumberOfExperiments(),
             binom = self.binomial(n, k),
             p = self.probability();
         return binom * Math.pow(p, k) * Math.pow(1 - p, n - k);
-    }
+    };
 
     self.binomial = function(n, k) {
         var numerator = self.factorial(n),
             denumerator = self.factorial(k) * self.factorial(n - k);
         return numerator / denumerator;
-    }
+    };
 
     self.factorial = function(x) {
         var result = 1;
         for (var i = 2; i <= x; i++)
             result *= i;
         return result;
-    }
+    };
+
+    self.renderChart = function(result) {
+        new Morris.Line({
+            element: 'result',
+            data: result,
+            xkey: 'ns',
+            ykeys: ['probability'],
+            labels: ['Probability'],
+            dateFormat: function(x) {return x.toString();}
+        });
+    };
 }
 
 $(function () {
